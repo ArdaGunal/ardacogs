9b44f4586677bed0742af0e051d193e6d24e788e
ArdaGunal <ardagunalfb2001@gmail.com>
2023-03-20 01:57:58 +0300

    dosya eklendi

--- KurabiyeCog/KurabiyeCog.py
+++ KurabiyeCog/KurabiyeCog.py
@@ -1,7 +1,6 @@
-import discord
-from discord.ext import commands
 import random
-import asyncio
+import discord
+from redbot.core import commands
 
 class KurabiyeCog(commands.Cog):
     def __init__(self, bot):
