91317be51a04d816299df6e89d2d1eb27d06c555
ArdaGunal <ardagunalfb2001@gmail.com>
2023-03-22 00:50:39 +0300

    dosya eklendi

--- kripto/kriptocog.py
+++ kripto/kriptocog.py
@@ -1,31 +1,48 @@
 import discord
 from redbot.core import commands
-import coinmarketcap
+from typing import List
+from requests.exceptions import HTTPError
 
-class Kriptocog(commands.Cog):
+class CryptoCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.api_key = "87b19629-91ab-4a66-a4e7-aee344b115ac" # CoinMarketCap API anahtarını buraya girin
 
-    @commands.command(name="kripto")
-    async def get_crypto(self, ctx, count: int = 10):
-        # CoinMarketCap API'ye bağlanın
-        client = coinmarketcap.CoinMarketCap(self.api_key)
+    @commands.command()
+    async def multicoin(self, ctx: commands.Context, *coins: str) -> None:
+        """
+        Gets the current USD value for a list of coins
+        `coins` must be a list of white space separated crypto coins
+        e.g. `[p]multicoin BTC BCH LTC ETH DASH XRP`
+        """
+        coin_list = await self.get_coins(coins) if coins else await self.get_latest_coins()
+        if not coin_list:
+            await ctx.send("The provided list of coins aren't acceptable.")
+            return
 
-        # İstenen sayıda kripto para birimini alın
-        data = client.ticker(limit=count)
+        if await ctx.embed_requested():
+            embed = discord.Embed(title="Crypto coin comparison")
+            for coin in coin_list[:25]:
+                price = coin.quote["USD"].price
+                msg = f"1 {coin.symbol} is {price:,.2f} USD"
+                embed.add_field(name=coin.name, value=msg)
+            await ctx.send(embed=embed)
+        else:
+            msg = ""
+            for coin in coin_list[:25]:
+                price = coin.quote["USD"].price
+                msg += f"1 {coin.symbol} is {price:,.2f} USD\n"
+            await ctx.send(msg)
 
-        # Verileri mesaj olarak formatlayın
-        message = f"İlk {count} kripto para birimi:\n\n"
-        for currency in data:
-            rank = currency['rank']
-            name = currency['name']
-            symbol = currency['symbol']
-            price = currency['price_usd']
-            message += f"{rank}. {name} ({symbol}): ${price}\n"
+    async def get_latest_coins(self) -> List:
+        try:
+            data = await self.bot.coinmarketcap.ticker(0, convert="USD")
+            return data
+        except HTTPError:
+            raise commands.CommandError("Failed to fetch coin data.")
 
-        # Mesajı gönderin
-        await ctx.send(message)
-
-def setup(bot):
-    bot.add_cog(CryptoCog(bot))
+    async def get_coins(self, coins: List[str]) -> List:
+        try:
+            data = await self.bot.coinmarketcap.ticker(coins, convert="USD")
+            return data
+        except HTTPError:
+            raise commands.CommandError("Failed to fetch coin data.")
