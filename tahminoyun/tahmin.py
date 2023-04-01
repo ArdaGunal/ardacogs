d586643358dd40a3defb646ec65023e7592a7140
ArdaGunal <ardagunalfb2001@gmail.com>
2023-04-02 02:22:33 +0300

    dosya eklendi

--- tahminoyun/tahmin.py
+++ tahminoyun/tahmin.py
@@ -1,75 +1,42 @@
+import random
 import discord
 from redbot.core import commands
-import random
 
 class Tahmin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.word = ""
-        self.guesses = []
-        self.max_attempts = 5
-        self.current_attempts = 0
-        self.win = False
+        self.words = ["apple", "banana", "cherry", "date", "elderberry"]
+        self.guesses = 7
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author == self.bot.user:
-            return
+    @commands.command()
+    async def start(self, ctx):
+        def check(m):
+            return m.author == ctx.author and m.channel == ctx.author.dm_channel
 
-        if message.content.lower() == "!startgame":
-            await message.author.send("Lütfen bir kelime veya cümle seçin ve bana özel mesaj olarak gönderin.")
-            self.guesses = []
-            self.current_attempts = 0
-            self.win = False
+        word = random.choice(self.words)
+        blanks = "_" * len(word)
+        await ctx.author.send(f"Kelime: {blanks}\nTahmin hakkınız: {self.guesses}\nKelimeyi tahmin etmek için lütfen harf ya da kelime yazın.")
 
-        elif message.content.lower() == "!endgame":
-            await message.channel.send("Oyun iptal edildi.")
-            self.word = ""
-            self.guesses = []
-            self.current_attempts = 0
-            self.win = False
+        while self.guesses > 0 and blanks != word:
+            try:
+                msg = await self.bot.wait_for("message", check=check, timeout=60)
+            except asyncio.TimeoutError:
+                await ctx.author.send("Süreniz doldu, oyunu kaybettiniz!")
+                return
 
-        elif message.content.lower() == "!guesses":
-            await message.channel.send(f"Kalan tahmin hakkınız: {self.max_attempts - self.current_attempts}\nTahminleriniz: {', '.join(self.guesses)}")
-
-        elif message.author == message.author.bot:
-            return
-
-        elif self.word == "":
-            self.word = message.content.lower()
-            if len(self.word.split()) > 1:
-                await message.channel.send(f"Cümle seçildi. Toplam {len(self.word.split())} kelime ve {len(self.word)} harf var.")
-                self.word = self.word.split()
+            guess = msg.content.lower()
+            if len(guess) == len(word) and guess == word:
+                await ctx.send(f"{ctx.author.mention} kazandın! Kelime: {word}")
+                return
+            elif len(guess) == 1 and guess in word:
+                indices = [i for i in range(len(word)) if word[i] == guess]
+                for i in indices:
+                    blanks = blanks[:i] + guess + blanks[i+1:]
+                await ctx.author.send(f"Doğru! Kelime: {blanks}")
             else:
-                await message.channel.send(f"Kelime seçildi. Toplam {len(self.word)} harf var.")
-                self.word = list(self.word)
-            await message.channel.send("Oyuna başlandı. Tahminlerinizi yapabilirsiniz.")
-
-        elif self.win:
-            return
-
+                self.guesses -= 1
+                await ctx.author.send(f"Yanlış tahmin! Kalan tahmin hakkınız: {self.guesses}")
+        if self.guesses == 0:
+            await ctx.author.send(f"Maalesef tahmin hakkınız bitti! Doğru cevap: {word}")
         else:
-            guess = message.content.lower()
-            if guess in self.guesses:
-                await message.channel.send("Bu harfi/daha önce bu kelimeyi zaten tahmin ettiniz. Lütfen başka bir harf/kelime deneyin.")
-            else:
-                self.guesses.append(guess)
-                if isinstance(self.word, list):
-                    if guess == "".join(self.word):
-                        await message.channel.send("Tebrikler! Doğru kelimeyi tahmin ettiniz.")
-                        self.win = True
-                    else:
-                        await message.channel.send("Maalesef yanlış kelime tahmini. Lütfen tekrar deneyin.")
-                else:
-                    if guess in self.word:
-                        await message.channel.send(f"Tebrikler! '{guess}' harfi doğru.")
-                        if all(letter in self.guesses for letter in self.word):
-                            await message.channel.send("Tebrikler! Tüm harfleri doğru tahmin ettiniz.")
-                            self.win = True
-                    else:
-                        await message.channel.send(f"Maalesef '{guess}' harfi yanlış. Lütfen tekrar deneyin.")
-                        self.current_attempts += 1
-                        if self.current_attempts == self.max_attempts:
-                            await message.channel.send("Maalesef tahmin hakkınız kalmadı. Kaybettiniz.")
-                            self.word = ""
-                            self.win = True
+            await ctx.send(f"{ctx.author.mention} kazandın! Kelime: {word}")
