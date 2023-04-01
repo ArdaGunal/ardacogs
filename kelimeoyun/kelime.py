89424e1600858f52d7ca159a073feb5647051abb
ArdaGunal <ardagunalfb2001@gmail.com>
2023-04-01 03:04:25 +0300

    dosya eklendi

--- kelimeoyun/kelime.py
+++ kelimeoyun/kelime.py
@@ -1,5 +1,6 @@
 import discord
-from discord.ext import commands
+from redbot.core import commands
+import random
 
 class Kelime(commands.Cog):
     def __init__(self, bot):
@@ -9,37 +10,47 @@ class Kelime(commands.Cog):
 
     @commands.command(name="kelimeoyunu")
     async def kelimeoyunu(self, ctx):
-        await ctx.author.send("Kelimeyi DM'den yazın:")
-        self.current_word = ""
+        author = ctx.author
+        dm_channel = await author.create_dm()
+        await dm_channel.send("Lütfen bir kelime yazın, bu kelimeyi diğer oyuncular tahmin edecek.")
+        def check(m):
+            return m.author == author and isinstance(m.channel, discord.DMChannel)
+        message = await self.bot.wait_for('message', check=check)
+        self.current_word = message.content.lower()
+        await ctx.send(f"{author.mention} tarafından seçilen kelime: {self.current_word}")
         self.guesses.clear()
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author.bot or not isinstance(message.channel, discord.DMChannel):
+        if message.author.bot:
             return
-
+        if isinstance(message.channel, discord.DMChannel):
+            return
         if self.current_word == "":
-            self.current_word = message.content.lower()
-            await message.author.send(f"Kelime seçildi: {self.current_word}. Tahminlerinizi kanala yazabilirsiniz.")
             return
-
-        guess = message.content.lower()
-        if guess == self.current_word:
-            await message.channel.send(f"Tebrikler, {message.author.mention}! Kelimeyi doğru bildiniz!")
+        if message.content.lower() == self.current_word:
+            await message.channel.send(f"Tebrikler, {message.author.mention} kelimeyi doğru bildiniz!")
             self.current_word = ""
-            self.guesses.clear()
             return
+        if message.content.lower() in self.guesses:
+            return
+        self.guesses.add(message.content.lower())
+        word_progress = []
+        for letter in self.current_word:
+            if letter.lower() in self.guesses:
+                word_progress.append(letter)
+            else:
+                word_progress.append("_")
+        await message.channel.send(f"{message.author.mention} tarafından yapılan tahmin: {' '.join(word_progress)}")
 
-        if guess in self.guesses:
-            await message.channel.send(f"{message.author.mention}, '{guess}' kelimesini zaten tahmin ettiniz!")
-        else:
-            self.guesses.add(guess)
-            await message.channel.send(f"{message.author.mention}, '{guess}' kelimesi yanlış!")
+        if "_" not in word_progress:
+            await message.channel.send(f"Tebrikler, {message.author.mention} kelimeyi doğru bildiniz!")
+            self.current_word = ""
 
     async def cog_command_error(self, ctx, error):
         if isinstance(error, commands.CommandInvokeError) and isinstance(error.original, discord.Forbidden):
-            await ctx.author.send("Kelime oyunu DM'den oynandığı için, sizinle özel mesaj yoluyla iletişim kuramıyorum. Lütfen DM'leri açın veya botu engellemeyin.")
+            await ctx.send("Kelime oyunu DM'den oynandığı için, sizinle özel mesaj yoluyla iletişim kuramıyorum. Lütfen DM'leri açın veya botu engellemeyin.")
         else:
-            await ctx.author.send(f"Bir hata oluştu: {error}")
+            await ctx.send(f"Bir hata oluştu: {error}")
         self.current_word = ""
         self.guesses.clear()
