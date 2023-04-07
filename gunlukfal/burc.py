1847d4f15a444cc3da6ae8a16bbe6465dc1d6ded
ArdaGunal <ardagunalfb2001@gmail.com>
2023-04-07 22:53:37 +0300

    dosya eklendi

--- gunlukfal/burc.py
+++ gunlukfal/burc.py
@@ -1,39 +1,50 @@
-# -*- coding: utf-8 -*-
+from datetime import datetime
+import pytz
 import discord
-import requests
+from discord.ext import commands, tasks
 from bs4 import BeautifulSoup
-import re
-from redbot.core import commands, tasks
+import requests
+
 
 class Burc(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.burclar = ['Koç', 'Boğa', 'İkizler', 'Yengeç', 'Aslan', 'Başak', 'Terazi', 'Akrep', 'Yay', 'Oğlak', 'Kova', 'Balık']
-        self.url = 'https://www.hurriyet.com.tr/astroloji/'
+        self.burclar = {
+            "koç": "1", "boğa": "2", "ikizler": "3",
+            "yengeç": "4", "aslan": "5", "başak": "6",
+            "terazi": "7", "akrep": "8", "yay": "9",
+            "oğlak": "10", "kova": "11", "balık": "12"
+        }
+        self.channel_id = 1048665677944336504
+        self.timezone = 'Europe/Istanbul'
+        self.prefix = "!"  # Burç Bot için kullanacağımız prefix
 
-    def get_burc_yorumlari(self):
-        response = requests.get(self.url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        yorumlar = soup.find_all('p', {'class': 'lead mb-5'})
-        burc_yorumlari = {}
-        for i, yorum in enumerate(yorumlar):
-            burc_yorumlari[self.burclar[i]] = yorum.text
-        return burc_yorumlari
+    async def get_burc_yorum(self, burc):
+        url = f"https://onedio.com/gunluk-burc-yorumlari/{self.burclar[burc]}"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        burc_yorumu = soup.find("div", class_="post-content").get_text().strip()
 
-    @commands.command(name='günlükburç', help='Günlük burç yorumlarını gösterir')
-    async def show_gunluk_burc(self, ctx):
-        burc_yorumlari = self.get_burc_yorumlari()
-        embed = discord.Embed(title='Günlük Burç Yorumları', description='Bugünün burç yorumları:', color=0xff5733)
-        for burc in self.burclar:
-            embed.add_field(name=burc, value=burc_yorumlari[burc], inline=False)
-        await ctx.send(embed=embed)
+        return burc_yorumu
 
-    @commands.command(name='burcum', help='Kendi burcunuzun günlük yorumunu gösterir')
-    async def show_burcum(self, ctx):
-        author = str(ctx.message.author)
-        response = requests.get(self.url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        burcum = soup.find('h2', {'class': 'sign-name'}).text.split()[0]
-        yorum = soup.find('p', {'class': 'lead mb-5'}).text
-        embed = discord.Embed(title='{} Burcu Günlük Yorumu'.format(burcum), description=yorum, color=0xff5733)
-        await ctx.send(embed=embed)
+    @commands.command(name="burçyorum")
+    async def burc_yorum(self, ctx, burc):
+        burc = burc.lower()
+        if burc not in self.burclar:
+            await ctx.send("Geçersiz burç girdiniz!")
+            return
+        burc_yorumu = await self.get_burc_yorum(burc)
+        await ctx.send(f"{burc.capitalize()} burcu günlük yorumu: {burc_yorumu}")
+
+    @tasks.loop(hours=24)
+    async def send_burc_yorum(self):
+        now = datetime.now(pytz.timezone(self.timezone))
+        if now.hour == 10 and now.minute == 0:
+            channel = self.bot.get_channel(self.channel_id)
+            for burc in self.burclar:
+                burc_yorumu = await self.get_burc_yorum(burc)
+                await channel.send(f"{burc.capitalize()} burcu günlük yorumu: {burc_yorumu}")
+
+    @send_burc_yorum.before_loop
+    async def before_send_burc_yorum(self):
+        await self.bot.wait_until_ready()
