3c9078c40e14220cfe1cce6153bac1038b27b9c5
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-04-08 10:04:16 +0300

    Update burc.py

--- gunlukfal/burc.py
+++ gunlukfal/burc.py
@@ -5,16 +5,22 @@ import requests
 class Burc(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
     async def get_burc_yorum(self, burc):
         url = f"https://www.elle.com.tr/astroloji/{burc}"
         page = requests.get(url)
         soup = BeautifulSoup(page.content, "html.parser")
         burc_yorumu = soup.find("div", class_="body-el-text standard-body-el-text").get_text().strip()
-        return burc_yorumu
-    @commands.command(name="gburç")
-    async def burc_yorum(self, ctx, burc):
+
+    @commands.command(name="burç")
+    async def burç(self, ctx, burc):
+        member = ctx.author
         burc = burc.lower()
         burc_yorumu = await self.get_burc_yorum(burc)
-        await ctx.send(f"{burc.capitalize()} burcu günlük yorumu: {burc_yorumu}")
+        embed = discord.Embed(title=f"{member.display_name}'nin günlük falı", color=0x00ff00)
+        embed.set_thumbnail(url=member.avatar_url)
+        embed.add_field(name=burc, value=burc_yorum )
+        embed.set_image(url=https://i.elle.com.tr/elle-test-images/elle_{burc}.jpg(size=512))
+        await ctx.send(embed=embed)
 
     
