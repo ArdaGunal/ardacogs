87adf5a8b8b67b73bd1e7e8a7a9c66a1f4be39cd
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2023-04-08 10:20:20 +0300

    Update burc.py

--- gunlukfal/burc.py
+++ gunlukfal/burc.py
@@ -18,7 +18,7 @@ class Burc(commands.Cog):
         member = ctx.author
         burc = burc.lower()
         burc_yorumu = await self.get_burc_yorum(burc)
-        "burc_url" = "https://i.elle.com.tr/elle-test-images/elle_{burc}.jpg"
+        "burc_url" = https://i.elle.com.tr/elle-test-images/elle_{burc}.jpg
         embed = discord.Embed(title=f"{member.display_name}'nin günlük falı", color=0x00ff00)
         embed.set_thumbnail(url=member.avatar_url)
         embed.add_field(name=burc, value=burc_yorumu )
