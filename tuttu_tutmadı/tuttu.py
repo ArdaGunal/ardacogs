8f1eaa19b4e5fd8ef151b3718fee6e207326a0b0
ArdaGunal <ardagunalfb2001@gmail.com>
2023-04-12 00:29:00 +0300

    dosya eklendi

--- tuttu_tutmadı/tuttu.py
+++ tuttu_tutmadı/tuttu.py
@@ -1,28 +1,39 @@
-import re
+from redbot.core import commands, checks, Config
 import discord
-from redbot.core import commands
 
 class Tuttu(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.channel = None
+        self.active = False
 
-    @commands.command()
-    async def tuttu(self, ctx, channel: discord.TextChannel):
-        def check(message):
-            return message.author == ctx.author and message.channel == ctx.channel
+    @commands.group()
+    async def tuttu(self, ctx):
+        """Tuttu tutmadı oyununu başlatır."""
+        if ctx.invoked_subcommand is None:
+            await ctx.send_help()
 
-        def check_tuttu_tutmadı(reaction, user):
-            return user != self.bot.user and reaction.message.channel == channel
+    @tuttu.command(name="kanal")
+    async def set_channel(self, ctx, channel: discord.TextChannel):
+        """Oyunun oynanacağı kanalı belirtir."""
+        self.channel = channel
+        await ctx.send(f"Oyun kanalı {channel.mention} olarak belirlendi.")
 
-       # await ctx.send("Oyun başladı. Kanalda biri `tuttu` dediğinde ✅, `tutmadı` dediğinde ❌ reaksiyonları eklenecektir.")
+    @tuttu.command(name="başla")
+    async def start_game(self, ctx):
+        """Oyunu başlatır."""
+        if self.channel is None:
+            await ctx.send("Oyunun oynanacağı kanalı belirlemediniz.")
+            return
+        self.active = True
+        await self.channel.send("Oyun başladı. Kanalda biri tuttu dediğinde ✅, tutmadı dediğinde ❌ reaksiyonları eklenecektir.")
 
-        while True:
-            try:
-                reaction, _ = await self.bot.wait_for("reaction_add", check=check_tuttu_tutmadı)
-            except asyncio.TimeoutError:
-                break
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if not self.active or message.channel != self.channel or message.author.bot:
+            return
 
-            if str(reaction.emoji) == "✅":
-                await reaction.message.add_reaction("✅")
-            elif str(reaction.emoji) == "❌":
-                await reaction.message.add_reaction("❌")
+        if message.content.lower() == "tuttu":
+            await message.add_reaction("✅")
+        elif message.content.lower() == "tutmadı":
+            await message.add_reaction("❌")
