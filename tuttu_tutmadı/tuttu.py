979989bd42a1863b72e3786ee2555ced555a081f
ArdaGunal <ardagunalfb2001@gmail.com>
2023-04-12 00:21:06 +0300

    dosya eklendi

--- tuttu_tutmadı/tuttu.py
+++ tuttu_tutmadı/tuttu.py
@@ -1,3 +1,4 @@
+import re
 import discord
 from redbot.core import commands
 
@@ -6,31 +7,22 @@ class Tuttu(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def Tuttu(self, ctx):
+    async def tuttu(self, ctx, channel: discord.TextChannel):
         def check(message):
             return message.author == ctx.author and message.channel == ctx.channel
 
-        await ctx.send("Oyunun oynanacağı kanalı belirtin.")
-        channel_msg = await self.bot.wait_for("message", check=check)
-
-        try:
-            channel = await commands.TextChannelConverter().convert(ctx, channel_msg.content)
-        except commands.ChannelNotFound:  # Hata adı düzeltildi
-            await ctx.send("Kanal bulunamadı.")
-            return
-        
-        def check_tuttu_tutmadı(message):
-            return message.author != self.bot.user and message.channel == channel
+        def check_tuttu_tutmadı(reaction, user):
+            return user != self.bot.user and reaction.message.channel == channel
 
         await ctx.send("Oyun başladı. Kanalda biri `tuttu` dediğinde ✅, `tutmadı` dediğinde ❌ reaksiyonları eklenecektir.")
-        
+
         while True:
             try:
-                message = await self.bot.wait_for("message", check=check_tuttu_tutmadı)
-            except:
-                return
-            
-            if "tuttu" in message.content.lower():
-                await message.add_reaction("✅")
-            elif "tutmadı" in message.content.lower():
-                await message.add_reaction("❌")
+                reaction, _ = await self.bot.wait_for("reaction_add", check=check_tuttu_tutmadı)
+            except asyncio.TimeoutError:
+                break
+
+            if str(reaction.emoji) == "✅":
+                await reaction.message.add_reaction("✅")
+            elif str(reaction.emoji) == "❌":
+                await reaction.message.add_reaction("❌")
