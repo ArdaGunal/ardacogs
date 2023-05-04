c402232d81eccea7e6107637ccd883e8b700c05d
ArdaGunal <ardagunalfb2001@gmail.com>
2023-05-04 20:25:31 +0300

    dosya eklendi

--- colorme/__init__.py
+++ colorme/__init__.py
@@ -1,9 +1,8 @@
 
-
 import json
 from pathlib import Path
 
-from    colorme import ColorMe
+from  .colorme import ColorMe
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
