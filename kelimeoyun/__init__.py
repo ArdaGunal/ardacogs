b8866c0f35c2c1af99e483f7dd667dd72982af9a
ArdaGunal <ardagunalfb2001@gmail.com>
2023-06-02 05:22:11 +0300

    dosya eklendi

--- kelimeoyun/__init__.py
+++ kelimeoyun/__init__.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
+import json
+from pathlib import Path
+
+from redbot.core.bot import Red
+
 from .kelime import Kelime
 
-def setup(bot):
-    bot.add_cog(Kelime(bot))
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+
+async def setup(bot:Red) -> None:
+    await bot.add_cog(Kelime(bot))
     
     
