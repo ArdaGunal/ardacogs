b12cfd782bb53a50edccdabedd5f824e4ef79579
ArdaGunal <ardagunalfb2001@gmail.com>
2023-06-07 21:40:53 +0300

    dosya eklendi

--- bumphatırlat/__init__.py
+++ bumphatırlat/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from redbot.core.bot import Red
 
-from .bump import BumpReminder
+from .bumphatırlat import BumpReminder
 
 with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
