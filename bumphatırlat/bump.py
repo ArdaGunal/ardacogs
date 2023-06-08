0b6876d62e9832f2a32200dff051a0d23a10dce7
ArdaGunal <ardagunalfb2001@gmail.com>
2023-06-08 16:36:50 +0300

    dosya eklendi

--- bumphatırlat/bump.py
+++ bumphatırlat/bump.py
@@ -1,4 +1,5 @@
 import asyncio
+import discord
 from redbot.core import commands
 
 class Bump(commands.Cog):
@@ -6,22 +7,21 @@ class Bump(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message):
         if message.author.bot:
             return
 
         if message.content.lower() == "/bump":
-            disboard_bot_id = "302050872383242240"  # DISBOARD botunun ID'si
+            disboard_bot_id = 302050872383242240  # DISBOARD botunun ID'si
             bump_channel = message.channel
 
-            def check_bump_success(m):
-                return m.author.id == disboard_bot_id and "Bump done" in m.content
+            def check_bump_success(m: discord.Message):
+                return str(m.author.id) == str(disboard_bot_id) and "Bump done" in m.content
 
             try:
                 bump_success_msg = await self.bot.wait_for("message", check=check_bump_success, timeout=10)
                 if bump_success_msg:
-                    await asyncio.sleep(10)  # 2 saat bekle (2 * 60 * 60 saniye) 7200
+                    await asyncio.sleep(7200)  # 2 saat bekle (2 * 60 * 60 saniye) 7200
                     await bump_channel.send("2 saat doldu, sunucuyu tekrar bump yapabilirsiniz!")
             except asyncio.TimeoutError:
                 pass
-
