f329e71bd5a513d41b53eb85434fd6e8e53ac934
Arda Günal <85851637+ArdaGunal@users.noreply.github.com>
2023-06-12 09:31:52 +0300

    Update say.py

--- say/say.py
+++ say/say.py
@@ -8,8 +8,8 @@ class Say(commands.Cog):
     @commands.command()
     async def say(self, ctx, channel: discord.TextChannel = None, *, message: str):
         if channel is None:
-            channel=ctx.channel
-            await ctx.send(message)
+            await channel=ctx.channel.send(message)
+
             await ctx.message.delete()
         else:
             await channel.send(message)
