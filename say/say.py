7ff9582d8893fc24b4cc31958aa1678dd2ddab1d
Arda Günal <85851637+ArdaGunal@users.noreply.github.com>
2023-06-12 19:44:13 +0300

    Update say.py

--- say/say.py
+++ say/say.py
@@ -1,15 +1,31 @@
 from redbot.core import commands
+
 import discord
 
 class Say(commands.Cog):
+
     def __init__(self, bot):
+
         self.bot = bot
 
     @commands.command()
+
     async def say(self, ctx, channel: discord.TextChannel = None, *, message: str):
+
         if channel is None:
-            await ctx.channel.send(message)
-            await ctx.message.delete()
+
+        message = message.replace(channel,'')
+
         else:
+
             await channel.send(message)
+
             await ctx.message.delete()
+
+    
+          
+
+   
+        
+        
+          
